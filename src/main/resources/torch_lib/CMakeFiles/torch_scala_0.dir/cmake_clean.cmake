896c314dd177b9d8dd503b0e2b4d423ccf6cc63b
nazar <sss>
2019-07-01 22:32:23 +0300

    mapping

---
+++ src/main/resources/torch_lib/CMakeFiles/torch_scala_0.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/torch_scala_0.dir/jnijavacpp.cpp.o"
+  "CMakeFiles/torch_scala_0.dir/java_torch_lib.cpp.o"
+  "libtorch_scala_0.pdb"
+  "libtorch_scala_0.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/torch_scala_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
